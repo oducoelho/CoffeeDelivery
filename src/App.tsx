04ff88d6612c94ea2d7c563d4575ee36b6b1882b
Eduardo Coelho <104034703+oducoelho@users.noreply.github.com>
2022-10-07 20:02:30 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/theme/default'
+export const App = () => {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
+    </ThemeProvider>
+  )
+}
