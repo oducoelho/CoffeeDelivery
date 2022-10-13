9d726802c3c51be318d5d6db706502b8c12cfbd7
Eduardo Coelho <104034703+oducoelho@users.noreply.github.com>
2022-10-13 20:53:33 -0300

    Delete src directory

--- src/hooks/useCart.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react'
-import { CartContext } from '../contexts/CartContext'
-
-export const useCart = () => {
-  const context = useContext(CartContext)
-  return context
-}
